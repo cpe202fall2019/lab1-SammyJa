a58a347b6e9eedbdf133fb158990042cca4e22c3
SammyJa <sam.rs.jain@gmail.com>
2019-09-27 19:37:36 -0700

    Whoops

--- location_tests.py
+++ location_tests.py
@@ -6,8 +6,13 @@ class TestLab1(unittest.TestCase):
     def test_repr(self):
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
-    
-    # Add more tests!
+
+    def test_repr_1(self):
+        loc = Location("Paris", 48.9, 2.4)
+        self.assertEqual(repr(loc),"Location('Paris', 48.9, 2.4)")
+    def test_repr_2(self):
+        loc = Location("Area 51", 35.3, -120.7)
+        self.assertEqual(repr(loc),"Location('Area 51', 35.3, -120.7)")
 
 if __name__ == "__main__":
         unittest.main()
